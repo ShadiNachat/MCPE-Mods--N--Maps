a9e80f6931972fc0118cbac431c4b7b2ba913507
ShadiNachat <shadinachat@gmail.com>
2014-07-23 11:31:08 -0700

    Added Js file for maps.html!

---
+++ js/mapscript.js
@@ -0,0 +1,5 @@
+/* Write JavaScript here */
+
+
+function CC_go(form) {var myindex=form.dest.selectedIndex
+window.open(form.dest.options[myindex].value, target="_parent", "toolbar=yes,scrollbars=yes,location=yes"); }
